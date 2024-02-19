368f61ac1ca8e783173d9d58c2107967de71b084
Awwab <mawwabkhank2006@gmail.com>
2024-02-19 21:12:30 +0500

    bank managing multiple account objects through dictionary to prevent account number issue

--- bank.py
+++ bank.py
@@ -1,34 +1,38 @@
 # Test program using accounts
-# Version 2, using a list of accounts
+# Version 3 using a dictionary of accounts
 
 # Bring in all the code from the Account class file
 from account import *
 
-# Start off with empty list of accounts
-accountsList = [ ]  
+accountsDict = {}
+nextAccountNumber = 0
 
-# Create two accounts
+# Create two accounts:
 oAccount = Account('Joe', 100, 'JoesPassword')
-accountsList.append(oAccount)
-print("Joe's account number is 0")
+joesAccountNumber = nextAccountNumber
+accountsDict[joesAccountNumber] = oAccount
+print('Account number for Joe is:', joesAccountNumber)
+nextAccountNumber = nextAccountNumber + 1
 
 oAccount = Account('Mary', 12345, 'MarysPassword')
-accountsList.append(oAccount)
-print("Mary's account number is 1")
+marysAccountNumber = nextAccountNumber
+accountsDict[marysAccountNumber] = oAccount
+print('Account number for Mary is:', marysAccountNumber)
+nextAccountNumber = nextAccountNumber + 1
 
-accountsList[0].show()
-accountsList[1].show()
+accountsDict[joesAccountNumber].show()
+accountsDict[marysAccountNumber].show()
 print()
 
 # Call some methods on the different accounts
 print('Calling methods of the two accounts ...')
-accountsList[0].deposit(50, 'JoesPassword')
-accountsList[1].withdraw(345, 'MarysPassword')
-accountsList[1].deposit(100, 'MarysPassword')
+accountsDict[joesAccountNumber].deposit(50, 'JoesPassword')
+accountsDict[marysAccountNumber].withdraw(345, 'MarysPassword')
+accountsDict[marysAccountNumber].deposit(100, 'MarysPassword')
 
 # Show the accounts
-accountsList[0].show()
-accountsList[1].show()
+accountsDict[joesAccountNumber].show()
+accountsDict[marysAccountNumber].show()
 
 # Create another account with information from the user
 print()
@@ -37,17 +41,19 @@ userBalance = input('What is the starting balance for this account? ')
 userBalance = int(userBalance)
 userPassword = input('What is the password you want to use for this account? ')
 oAccount = Account(userName, userBalance, userPassword)
-accountsList.append(oAccount)  # append to list of accounts
+newAccountNumber = nextAccountNumber
+accountsDict[newAccountNumber] = oAccount
+print('Account number for new account is:', newAccountNumber)
+nextAccountNumber = nextAccountNumber + 1
 
 # Show the newly created user account
-print('Created new account, account number is 2')
-accountsList[2].show()
+accountsDict[newAccountNumber].show()
 
 # Let's deposit 100 into the new account
-accountsList[2].deposit(100, userPassword)
-usersBalance = accountsList[2].getBalance(userPassword)
+accountsDict[newAccountNumber].deposit(100, userPassword)
+usersBalance = accountsDict[newAccountNumber].getBalance(userPassword)
 print()
 print("After depositing 100, user's balance is:", usersBalance)
 
 # Show the new account
-accountsList[2].show()
+accountsDict[newAccountNumber].show()
