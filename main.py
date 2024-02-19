bc45a6755a9dd824e6c265c23c1668f44c3c4a50
Awwab <mawwabkhank2006@gmail.com>
2024-02-19 21:49:50 +0500

    a user interface for using bank class

--- main.py
+++ main.py
@@ -1,136 +1,61 @@
-# Non-OOP Bank
-# Version 5
-# Any number of accounts - with a list of dictionaries
+# Main program for controlling a Bank made up of Accounts
 
-accountsList = []
+# Bring in all the code of the Bank class
+from bank import *
 
-def newAccount(aName, aBalance, aPassword):
-    global accountsList
-    newAccountDict = {'name':aName, 'balance':aBalance, 'password':aPassword}
-    accountsList.append(newAccountDict)
-   
-def show(accountNumber):
-    global accountsList
-    print('Account', accountNumber)
-    thisAccountDict = accountsList[accountNumber]
-    print('       Name', thisAccountDict['name'])
-    print('       Balance:', thisAccountDict['balance'])
-    print('       Password:', thisAccountDict['password'])
-    print()
+# Create an instance of the Bank
+oBank = Bank()
 
-def getBalance(accountNumber, password):
-    global accountsList
-    thisAccountDict = accountsList[accountNumber]
-    if password != thisAccountDict['password']:
-        print('Incorrect password')
-        return None
-    return thisAccountDict['balance']
+# Main code
+# Create two test accounts
+joesAccountNumber = oBank.createAccount('Joe', 100, 'JoesPassword')
+print("Joe's account number is:", joesAccountNumber)
 
-def deposit(accountNumber, amountToDeposit, password):
-    global accountsList
-    thisAccountDict = accountsList[accountNumber]
-    if amountToDeposit < 0:
-        print('You cannot deposit a negative amount!')
-        return None
-        
-    if password != thisAccountDict['password']:
-        print('Incorrect password')
-        return None
-    
-    thisAccountDict['balance'] = thisAccountDict['balance'] + amountToDeposit
-    return thisAccountDict['balance']
-    
-def withdraw(accountNumber, amountToWithdraw, password):
-    global accountsList
-    thisAccountDict = accountsList[accountNumber]
-    if amountToWithdraw < 0:
-        print('You cannot withdraw a negative amount')
-        return None
-
-    if password != thisAccountDict['password']:
-        print('Incorrect password for this account')
-        return None
-
-    if amountToWithdraw > thisAccountDict['balance']:
-        print('You cannot withdraw more than you have in your account')
-        return None
-
-    thisAccountDict['balance'] = thisAccountDict['balance'] - amountToWithdraw
-    return thisAccountDict['balance']
-
-# Create two sample accounts
-print("Joe's account is account number:", len(accountsList))
-newAccount("Joe", 100, 'soup')
-
-print("Mary's account is account number:", len(accountsList))
-newAccount("Mary", 12345, 'nuts')
+marysAccountNumber = oBank.createAccount('Mary', 12345, 'MarysPassword')
+print("Mary's account number is:", marysAccountNumber)
 
 while True:
     print()
-    print('Press b to get the balance')
-    print('Press d to make a deposit')
-    print('Press n to create a new account')
-    print('Press w to make a withdrawal')
-    print('Press s to show all accounts')
-    print('Press q to quit')
+    print('To get an account balance, press b')
+    print('To close an account, press c')
+    print('To make a deposit, press d')
+    print('To get bank information, press i')
+    print('To open a new account, press o')
+    print('To quit, press q')
+    print('To show all accounts, press s')
+    print('To make a withdrawal, press w')
     print()
 
     action = input('What do you want to do? ')
-    action = action.lower()  # force lowercase
-    action = action[0]  # just use first letter
+    action = action.lower()
+    action = action[0]  # grab the first letter
     print()
     
     if action == 'b':
-        print('Get Balance:')
-        userAccountNumber = input('Please enter your account number: ')
-        userAccountNumber = int(userAccountNumber)
-        userPassword = input('Please enter the password: ')
-        theBalance = getBalance(userAccountNumber, userPassword)
-        if theBalance is not None:
-            print('Your balance is:', theBalance)
+        oBank.balance()
+
+    elif action == 'c':
+        oBank.closeAccount()
 
     elif action == 'd':
-        print('Deposit:')
-        userAccountNumber= input('Please enter the account number: ')
-        userAccountNumber = int(userAccountNumber)
-        userDepositAmount = input('Please enter amount to deposit: ')
-        userDepositAmount = int(userDepositAmount)
-        userPassword = input('Please enter the password: ')
+        oBank.deposit()
 
-        newBalance = deposit(userAccountNumber, userDepositAmount, userPassword)
-        if newBalance is not None:
-            print('Your new balance is:', newBalance)
-        
-    elif action == 'n':
-        print('New Account:')
-        userName = input('What is your name? ')
-        userStartingAmount = input('What is the amount of your initial deposit? ')
-        userStartingAmount = int(userStartingAmount)
-        userPassword = input('What password would you like to use for this account? ')
+    elif action == 'i':
+        oBank.bankInfo()
 
-        userAccountNumber = len(accountsList)
-        newAccount(userName, userStartingAmount, userPassword)
-        print('Your new account number is:', userAccountNumber)
+    elif action == 'o':
+        oBank.openAccount()
 
-    elif action == 's':   #show all
-        print('Show:')
-        nAccounts = len(accountsList)
-        for accountNumber in range(0, nAccounts):
-            show(accountNumber)
+    elif action == 's':
+        oBank.show()
 
     elif action == 'q':
         break
 
     elif action == 'w':
-        print('Withdraw:')
-        userAccountNumber = input('Please enter your account number: ')
-        userAccountNumber = int(userAccountNumber)
-        userWithdrawAmount = input('Please enter the amount to withdraw: ')
-        userWithdrawAmount = int(userWithdrawAmount)
-        userPassword = input('Please enter the password: ')
- 
-        newBalance = withdraw(userAccountNumber, userWithdrawAmount, userPassword)
-        if newBalance is not None:
-            print('Your new balance is:', newBalance)       
+        oBank.withdraw()
 
+    else:
+        print('Sorry, that was not a valid action.  Please try again.')
+        
 print('Done')
