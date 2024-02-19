2abf19010ed3a077bc9a12424f58ac2b0f4f49f0
Awwab <mawwabkhank2006@gmail.com>
2024-02-19 21:11:09 +0500

    bank manage multiple account objects using list

--- bank.py
+++ bank.py
@@ -1,29 +1,34 @@
 # Test program using accounts
-# Version 1, using explicit variables for each Account object
+# Version 2, using a list of accounts
 
 # Bring in all the code from the Account class file
 from account import *
 
+# Start off with empty list of accounts
+accountsList = [ ]  
+
 # Create two accounts
-oJoesAccount = Account('Joe', 100, 'JoesPassword')
-print("Created an account for Joe")
+oAccount = Account('Joe', 100, 'JoesPassword')
+accountsList.append(oAccount)
+print("Joe's account number is 0")
 
-oMarysAccount = Account('Mary', 12345, 'MarysPassword')
-print("Created an account for Mary")
+oAccount = Account('Mary', 12345, 'MarysPassword')
+accountsList.append(oAccount)
+print("Mary's account number is 1")
 
-oJoesAccount.show()
-oMarysAccount.show()
+accountsList[0].show()
+accountsList[1].show()
 print()
 
 # Call some methods on the different accounts
 print('Calling methods of the two accounts ...')
-oJoesAccount.deposit(50, 'JoesPassword')
-oMarysAccount.withdraw(345, 'MarysPassword')
-oMarysAccount.deposit(100, 'MarysPassword')
+accountsList[0].deposit(50, 'JoesPassword')
+accountsList[1].withdraw(345, 'MarysPassword')
+accountsList[1].deposit(100, 'MarysPassword')
 
 # Show the accounts
-oJoesAccount.show()
-oMarysAccount.show()
+accountsList[0].show()
+accountsList[1].show()
 
 # Create another account with information from the user
 print()
@@ -31,16 +36,18 @@ userName = input('What is the name for the new user account? ')
 userBalance = input('What is the starting balance for this account? ')
 userBalance = int(userBalance)
 userPassword = input('What is the password you want to use for this account? ')
-oNewAccount = Account(userName, userBalance, userPassword)
+oAccount = Account(userName, userBalance, userPassword)
+accountsList.append(oAccount)  # append to list of accounts
 
 # Show the newly created user account
-oNewAccount.show()
+print('Created new account, account number is 2')
+accountsList[2].show()
 
 # Let's deposit 100 into the new account
-oNewAccount.deposit(100, userPassword)
-usersBalance = oNewAccount.getBalance(userPassword)
+accountsList[2].deposit(100, userPassword)
+usersBalance = accountsList[2].getBalance(userPassword)
 print()
-print("After depositing 100, user's balance is", usersBalance)
+print("After depositing 100, user's balance is:", usersBalance)
 
 # Show the new account
-oNewAccount.show()
+accountsList[2].show()
