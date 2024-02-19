0497880254d4904384c4e615020423b98abb1669
Awwab <mawwabkhank2006@gmail.com>
2024-02-19 21:52:55 +0500

    interface to show exceptions if occured

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 # Main program for controlling a Bank made up of Accounts
-
-# Bring in all the code of the Bank class
 from bank import *
-
+   
 # Create an instance of the Bank
-oBank = Bank()
+oBank = Bank('9 to 5', '123 Main Street, Anytown, USA', '(650) 555-1212')
 
-# Main code
-# Create two test accounts
-joesAccountNumber = oBank.createAccount('Joe', 100, 'JoesPassword')
-print("Joe's account number is:", joesAccountNumber)
-
-marysAccountNumber = oBank.createAccount('Mary', 12345, 'MarysPassword')
-print("Mary's account number is:", marysAccountNumber)
-
+#Main code
 while True:
     print()
     print('To get an account balance, press b')
@@ -23,39 +14,33 @@ while True:
     print('To open a new account, press o')
     print('To quit, press q')
     print('To show all accounts, press s')
-    print('To make a withdrawal, press w')
+    print('To make a withdrawal, press w ')
     print()
 
     action = input('What do you want to do? ')
     action = action.lower()
     action = action[0]  # grab the first letter
     print()
-    
-    if action == 'b':
-        oBank.balance()
 
-    elif action == 'c':
-        oBank.closeAccount()
-
-    elif action == 'd':
-        oBank.deposit()
-
-    elif action == 'i':
-        oBank.bankInfo()
-
-    elif action == 'o':
-        oBank.openAccount()
-
-    elif action == 's':
-        oBank.show()
-
-    elif action == 'q':
-        break
-
-    elif action == 'w':
-        oBank.withdraw()
-
-    else:
-        print('Sorry, that was not a valid action.  Please try again.')
+    try:
+        if action == 'b':
+            oBank.balance()
+        elif action == 'c':
+            oBank.closeAccount()
+        elif action == 'd':
+            oBank.deposit()
+        elif action == 'i':
+            oBank.getInfo()
+        elif action == 'o':
+            oBank.openAccount()
+        elif action == 'q':
+            break
+        elif action == 's':
+            oBank.show()
+        elif action == 'w':
+            oBank.withdraw()
+    except AbortTransaction as error:
+        # Print out the text of the error message
+        print(error)
         
 print('Done')
